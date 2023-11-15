1079099b467e4b1f6cca7cd410379bad1cba9680
lass6230 <due6230@gmail.com>
2023-11-15 10:11:26 +0100

    asd

--- src/spot_safe_rl/spot_safe_rl/safe_rl_node.py
+++ src/spot_safe_rl/spot_safe_rl/safe_rl_node.py
@@ -4,12 +4,17 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from .submodules import LB_optimizer as LB
 
-
+from sensor_msgs.msg import LaserScan
 
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import Twist
+import tf2_py
+from tf_transformations import euler_from_quaternion
+
+# from rclpy.qos_event import SubscriptionEventCallbacks
+# from rclpy.qos_event import QoSSubscriptionEventType
 
 class SafeRlNode(Node):
 
@@ -18,14 +23,31 @@ class SafeRlNode(Node):
         # self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
 
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.target_frame = "base_link"
         self.from_frame = "map"
+
+        qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
+                                          history=rclpy.qos.HistoryPolicy.KEEP_LAST,
+                                          depth=1)
+        self.subscription = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.sensor_callback,
+            qos_profile=qos_policy,
+        )
+        self.subscription  # prevent unused variable warning
+
         # self.i = 0
         # sim = LB.Simulation()
 
+    def sensor_callback(self, msg):
+        print("sensor callback")
+        self.get_logger().info('Number of points: "%i"' % len(msg.ranges))
+
+        self.timer_callback()
 
     def timer_callback(self):
         # msg = String()
@@ -41,6 +63,15 @@ class SafeRlNode(Node):
                 rclpy.time.Time())
             self.get_logger().info(
                         f'got transform {self.target_frame} to {self.target_frame}')
+            self.get_logger().info('x: "%f"' % t.transform.translation.x)
+            self.get_logger().info('y: "%f"' % t.transform.translation.y)
+            orientation_list = [t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w]
+
+            (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+
+            self.pose_yaw = yaw
+
+            self.get_logger().info('z_rot: "%f"' % yaw)
         except TransformException as ex:
             self.get_logger().info(
                 f'Could not transform {self.target_frame} to {self.target_frame}: {ex}')
