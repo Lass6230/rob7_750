5658e21c0982e67b8bd6eb17ad89db3ce2252f3d
lass6230 <due6230@gmail.com>
2023-12-06 10:01:22 +0100

    asd

--- src/spot_safe_rl/spot_safe_rl/safe_rl_node.py
+++ src/spot_safe_rl/spot_safe_rl/safe_rl_node.py
@@ -27,8 +27,8 @@ class SafeRlNode(Node):
 
         self.goal_counter = 0
         self.medium_room_goals_ = [[10.8,4.5],[10.8,-4.5],[0.0,-4.5],[10.8,4.5]]
-
-
+        self.big_room_goals_ = [[14.5, -7 , 0.0],[14.5,7],[0.0,7,0.0],[14.5, -7,0.0],[0.0,-7,0.0],[0.0,0.0,0.0],[14.5,7,0.0],[0.0,-7,0.0]]
+        self.small_room_goals_ = [[],[]]
         buffer_size = 5
         self.cir_buffer_x_vel = collections.deque(maxlen=buffer_size)
         self.cir_buffer_y_vel = collections.deque(maxlen=buffer_size)
